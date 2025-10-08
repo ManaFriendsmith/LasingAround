aab2a46f0d26333830b3d55f9cfd7e9b24f32368
ManaFriendsmith <42ah42@gmail.com>
2025-10-07 22:12:57 -0400

    Add inventory sounds to some items that lacked them

--- prototypes/mill.lua
+++ prototypes/mill.lua
@@ -198,6 +198,9 @@ data:extend({
     subgroup = "production-machine",
     place_result = "laser-mill",
     order = "c[laser-mill]",
+    inventory_move_sound = item_sounds.mechanical_inventory_move,
+    pick_sound = item_sounds.mechanical_inventory_pickup,
+    drop_sound = item_sounds.mechanical_inventory_move,
     stack_size = 10,
     weight = 100 * kg
   },
