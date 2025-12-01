ddb7a662c794ba3878200ec24f55e556d2935f50
ManaFriendsmith <42ah42@gmail.com>
2025-12-01 15:50:59 -0500

    lunar landings compat

--- compat/small-mod.lua
+++ compat/small-mod.lua
@@ -2,7 +2,7 @@ local rm = require("__pf-functions__/recipe-manipulation")
 local tm = require("__pf-functions__/technology-manipulation")
 local misc = require("__pf-functions__/misc")
 
-if mods["se-space-trains"] then
+if mods["se-space-trains"] and not mods["LunarLandings"] then
     if misc.difficulty == 3 and mods["space-age"] then
         rm.ReplaceIngredientProportional("space-locomotive", "processing-unit", "ai-girlfriend")
         tm.AddPrerequisite("tech-space-trains", "waifugenesis")
